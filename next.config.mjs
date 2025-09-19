17334111193fbd97e57e006633152cd6ace1ee0f
unknown <renanacademic21@gmail.com>
2025-09-19 17:57:23 -0300

    feat: Adiciona domínio R2 ao next.config.js para carregamento de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['pub-03b33c3cc01341c68dcb40c656d81b34.r2.dev'], // <--- ADICIONE ESTA LINHA!
+  },
   compiler: {
     styledComponents: true,
   },
